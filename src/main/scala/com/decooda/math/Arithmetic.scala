7ba5ab692512c209c797e0702b86276d28b4c710
Suri1596 <surekha.k@techsophy.com>
2019-01-12 16:55:09 +0530

    Project setup

---
+++ src/main/scala/com/decooda/math/Arithmetic.scala
@@ -0,0 +1,9 @@
+package com.decooda.math
+
+class Arithmetic {
+
+  def sum(a: Int, b: Int): Int = {
+    a + b
+  }
+
+}
---
+++ src/test/scala/com/decooda/math/ArithmeticTest.scala
@@ -0,0 +1,16 @@
+package com.decooda.math
+
+import org.scalatest.FunSuite
+
+class ArithmeticTest extends FunSuite {
+
+
+  val arth = new Arithmetic
+
+  test("Sum two integers ") {
+    val result = arth.sum(2, 1)
+    val expectResult = 5
+    assert(result === expectResult)
+  }
+
+}
