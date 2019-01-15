899d6780096484ea3be5a7d8da182ab16eb87ded
Suri1596 <surekha.k@techsophy.com>
2019-01-15 18:00:26 +0530

    Arithmetic operations On list

--- src/main/scala/edu/hcu/math/Arithmetic.scala
+++ src/main/scala/edu/hcu/math/Arithmetic.scala
@@ -1,9 +1,84 @@
 package edu.hcu.math
 
 class Arithmetic {
-
+  //sum of two numbers
   def sum(a: Int, b: Int): Int = {
     a + b
   }
+
+
+  //factorial of a number
+  def factorial(n: Int): Int = {
+    if (n == 0)
+      1
+    else
+      n * factorial(n - 1)
+  }
+
+
+  // 1.Max element in a list
+  def findMax(x: List[Int]): Int = {
+    def findMax(x: List[Int], max: Int): Int = {
+      if (x.nonEmpty) {
+        if (x.head > max) findMax(x.tail, x.head) else findMax(x.tail, max)
+      }
+      else
+        max
+    }
+
+    findMax(x.tail, x.head)
+  }
+
+  //2.find max element using foldLeft
+  def max(list: List[Int]): Int = {
+    list.tail.foldLeft(0) { (max, ele) => if (max > ele) max else ele }
+  }
+
+  //3.Reverse of a list
+  def reverse(list: List[Int]): List[Int] = {
+    list.foldLeft(List[Int]()) { (temp, ele) => if (list.nonEmpty) ele :: temp else temp }
+  }
+
+
+  //* 4.distinct element of a list
+  def dedupe(list: List[Int]): List[Int] = {
+    list.foldLeft(List[Int]()) { (temp, ele) => if (!temp.contains(ele)) temp :+ ele else temp }
+
+  }
+
+  // 5.Max In a List Using Match Pattern
+  def max2(list: List[Int]): Int = {
+    list match {
+      case Nil => 0
+      case List(x: Int) => x
+      case x :: tail => if (x > max2(tail)) x else max2(tail)
+
+    }
+  }
+
+  //6. Sum of a List Using Pattern Matching
+  def sumL(list: List[Int]): Int = {
+    list match {
+      case Nil => 0
+      case List(x: Int) => x
+      case x :: tail => x + sumL(tail)
+    }
+  }
+
+  /*merging two map*/
+  def mergeMap(map1: Map[String, Int], map2: Map[String, Int]): Map[String, Int] = {
+    val merge = (map1.keySet ++ map2.keySet).toList.map { key => (key, (map1.getOrElse(key, 0) + map2.getOrElse(key, 0))) }.toMap
+
+    merge
+  }
+
+  /*Merging two Lists*/
+  def merge(list1: List[Int], list2: List[Int]): List[Int] = {
+    val mutableList = collection.mutable.ListBuffer[Int]()
+    list1.foreach(n => mutableList.append(n))
+    list2.foreach(n => mutableList.append(n))
+    mutableList.toList
+  }
+
 
 }
--- src/test/scala/edu/hcu/math/ArithmeticTest.scala
+++ src/test/scala/edu/hcu/math/ArithmeticTest.scala
@@ -13,4 +13,65 @@ class ArithmeticTest extends FunSuite {
     assert(result === expectResult)
   }
 
+
+  test("Factorial of a number") {
+    val result = arth.factorial(4)
+    val expectResult = 24
+    assert(result === expectResult)
+  }
+
+  //1st
+  test("Max in a list") {
+    val result = arth.findMax(List(2, 3, 5, 89, 33))
+    val expectResult = 89
+    assert(result === expectResult)
+  }
+
+  //2nd
+  test("Max in a list using foldLeft") {
+    val result = arth.max(List(2, 3, 5, 89, 33, 100))
+    val expectResult = 100
+    assert(result === expectResult)
+  }
+
+  //3rd
+  test("Reverse of a list") {
+    val result = arth.reverse(List(1, 2, 2, 3, 3, 4, 4, 4))
+    val expectResult = List(4, 4, 4, 3, 3, 2, 2, 1)
+    assert(result === expectResult)
+  }
+
+
+  //4th
+  test("Distinct list") {
+    val result = arth.dedupe(List(1, 2, 2, 3, 3, 4, 4, 4))
+    val expectResult = List(1, 2, 3, 4)
+    assert(result === expectResult)
+  }
+
+  //5th
+  test("Max in a list using Pattern Matching") {
+    val result = arth.max2(List(2, 3, 5, 89, 330, 10, 200))
+    val expectResult = 330
+    assert(result === expectResult)
+  }
+  //6th
+  test("Sum of a list using Pattern Matching") {
+    val result = arth.sumL(List(2, 3, 5, 89, 330, 10, 200))
+    val expectResult = 639
+    assert(result === expectResult)
+  }
+
+
+  test("Merging two Maps") {
+    val result = arth.mergeMap(Map("a" -> 1, "b" -> 3), Map("a" -> 2))
+    val expectResult = Map("a" -> 3, "b" -> 3)
+    assert(result === expectResult)
+  }
+
+  test("Merging two Lists") {
+    val result = arth.merge(List(1, 2, 3, 3, 4, 4), List(1, 2, 3, 4, 5))
+    val expectResult = List(1, 2, 3, 3, 4, 4, 1, 2, 3, 4, 5)
+    assert(result === expectResult)
+  }
 }
